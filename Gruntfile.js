6f0e436766672336edf97a45d5e0b0b76c9777d7
Lalit Tanwar <lalit.tanwar07@gmail.com>
2015-08-08 18:08:19 +0530

    grunt updates

--- Gruntfile.js
+++ Gruntfile.js
@@ -25,7 +25,7 @@ module.exports = function (grunt) {
 
     uglify: {
       options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+       // banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
       },
       my_target: {
         options: {
